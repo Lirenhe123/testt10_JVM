e93443d9d1f015194930d5173036c12812293797
lirenhe <1017621207@qq.com>
2017-11-19 15:23:20 +0800

    the first commit

---
+++ src/priv/lirenhe/classloader/MyTest01.java
@@ -0,0 +1,44 @@
+package priv.lirenhe.classloader;
+
+class Singleton
+{
+	private static Singleton singleton = new Singleton();
+	public static int counter1;
+	public static int counter2 = 0;
+	
+	private Singleton()
+	{
+		counter1++;
+		counter2++;
+	}
+	public static Singleton getInstance()
+	{
+		return singleton;
+	}
+}
+public class MyTest01
+{
+	public static void main(String[] args)
+	{
+		Singleton singleton = Singleton.getInstance();
+		System.out.println("counter1 = " + singleton.counter1);
+		System.out.println("counter2 = " + singleton.counter2);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
